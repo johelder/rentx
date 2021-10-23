6f92ec4f79d3fa7c930d74dfa7ded74c65a5f2bd
johelder <johelder.arj@gmail.com>
2021-10-23 10:29:56 -0300

    adding fonts to app file

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import AppLoading from 'expo-app-loading';
+
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo';
+
+import { Home } from './pages/Home';
+
+import { ThemeProvider } from 'styled-components/native';
+import theme from './styles/theme';
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, Inter_500Medium, Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold
+  })
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
+}
