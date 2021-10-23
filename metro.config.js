4a58af7a3a288600ff69ac4dae34996fbdd1e8d8
johelder <johelder.arj@gmail.com>
2021-10-23 19:40:03 -0300

    configure react native svg transformer

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,3 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("expo/metro-config");
-
 const { getDefaultConfig } = require("expo/metro-config");
 
 module.exports = (async () => {
