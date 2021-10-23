207963d7e7ce1d37087eec1b737b38f58e7dc98b
johelder <johelder.arj@gmail.com>
2021-10-23 19:41:37 -0300

    rendering Car component on page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import { Car } from '../../Components/Car';
+
+import Logo from '../../assets/logo.svg';
+
 import * as S from './styles';
 
 export function Home(){
+
+  const carData = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png',
+  }
+
   return (
     <S.Container>
-      <Text>Home</Text>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <S.Header>
+        <S.HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <S.TotalCars>Total de 12 carros</S.TotalCars>
+        </S.HeaderContent>
+      </S.Header>
+
+      <S.CarList
+        data={[1, 2, 3, 4, 5, 6, 7, 8]}
+        keyExtractor={item => String(item)}
+        renderItem={({item}) => <Car data={carData} />}
+      />
+
     </S.Container>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,10 +1,40 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
 
-  align-items: center;
-  justify-content: center;
-
-  background-color: ${({ theme }) => theme.colors.main};
+  background-color: ${({ theme }) => theme.colors.background_primary};
 `;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+
+  background-color: ${({ theme }) => theme.colors.header};
+`;
+
+export const HeaderContent = styled.View`
+  width: 100%;
+  height: 100%;
+
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: space-between;
+
+  padding: 32px 24px;
+`;
+
+export const TotalCars = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  font-size: ${RFValue(15)}px;
+
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+export const CarList = styled.FlatList.attrs({
+  contentContainerStyle: {
+    padding: 24,
+  },
+  showsVerticalScrollIndicator: false,
+})``;
