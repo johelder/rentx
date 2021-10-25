f460b9a2ad204a7768504e9487883a3d11a1e877
johelder <johelder.arj@gmail.com>
2021-10-25 18:42:15 -0300

    adding new components to car details page

--- src/pages/CarDetails/index.tsx
+++ src/pages/CarDetails/index.tsx
@@ -1,14 +1,60 @@
 import React from 'react';
-import { BackButton } from '../../Components/BackButton';
+
+import { Acessory } from '../../components/Acessory';
+import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+
+import speedIcon from '../../assets/speed.svg';
+import accelerationIcon from '../../assets/acceleration.svg';
+import forceIcon from '../../assets/force.svg';
+import gasolineIcon from '../../assets/gasoline.svg';
+import exchangeIcon from '../../assets/exchange.svg';
+import peopleIcon from '../../assets/people.svg';
 
 import * as S from './styles';
+import { Button } from '../../components/Button';
 
 export function CarDetails() {
   return (
     <S.Container>
       <S.Header>
-        <BackButton />
+        <BackButton onPress={() => {}} />
       </S.Header>
+
+      <S.CarImages>
+        <ImageSlider imagesUrl={['https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png']} />
+      </S.CarImages>
+
+      <S.Content>
+        <S.Details>
+          <S.Description>
+            <S.Brand>Lamborghini</S.Brand>
+            <S.Name>Huracan</S.Name>
+          </S.Description>
+
+          <S.Rent>
+            <S.Period>Ao Dia</S.Period>
+            <S.Price>R$ 580</S.Price>
+          </S.Rent>
+        </S.Details>
+
+        <S.Acessories>
+          <Acessory icon={speedIcon} name="380Km/h" />
+          <Acessory icon={accelerationIcon} name="3.2s" />
+          <Acessory icon={forceIcon} name="800 HP" />
+          <Acessory icon={gasolineIcon} name="Gasolina" />
+          <Acessory icon={exchangeIcon} name="Auto" />
+          <Acessory icon={peopleIcon} name="2 pessoas" />
+        </S.Acessories>
+
+        <S.About>
+        Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+        </S.About>
+      </S.Content>
+
+      <S.Footer>
+        <Button title="Confirmar" />
+      </S.Footer>
     </S.Container>
   );
 }
--- src/pages/CarDetails/styles.ts
+++ src/pages/CarDetails/styles.ts
@@ -1,3 +1,8 @@
+import {
+  getBottomSpace,
+  getStatusBarHeight,
+} from "react-native-iphone-x-helper";
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
@@ -10,4 +15,98 @@ export const Header = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: center;
+
+  position: absolute;
+  margin-top: ${getStatusBarHeight() + 18}px;
+  margin-left: 24px;
+`;
+
+export const CarImages = styled.View`
+  margin-top: ${getStatusBarHeight() + 32}px;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    padding: 24,
+    alignItems: "center",
+  },
+  showsVerticalScrollIndicator: false,
+})``;
+
+export const Details = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 30px;
+`;
+
+export const Description = styled.View``;
+
+export const Brand = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  font-size: ${RFValue(10)}px;
+
+  text-transform: uppercase;
+
+  color: ${({ theme }) => theme.colors.text_detail};
+`;
+
+export const Name = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  font-size: ${RFValue(25)}px;
+
+  color: ${({ theme }) => theme.colors.title};
+`;
+
+export const Rent = styled.View``;
+
+export const Period = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  font-size: ${RFValue(10)}px;
+
+  text-transform: uppercase;
+
+  color: ${({ theme }) => theme.colors.text_detail};
+`;
+
+export const Price = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  font-size: ${RFValue(25)}px;
+
+  color: ${({ theme }) => theme.colors.main};
+`;
+
+export const About = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_400};
+  font-size: ${RFValue(15)}px;
+
+  text-align: justify;
+  line-height: ${RFValue(25)}px;
+
+  color: ${({ theme }) => theme.colors.text};
+
+  margin-top: 23px;
+`;
+
+export const Acessories = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 16px;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+
+  padding: 24px 24px ${getBottomSpace() + 24}px;
+
+  background-color: ${({ theme }) => theme.colors.background_primary};
 `;
