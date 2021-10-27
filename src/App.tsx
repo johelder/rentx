73269e01eb192a6a9b4cfa9b4bab6c75a382e32b
johelder <johelder.arj@gmail.com>
2021-10-26 21:59:28 -0300

    using Scheduling page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@exp
 
 import { Home } from './pages/Home';
 import { CarDetails } from './pages/CarDetails';
+import { Scheduling } from './pages/Scheduling';
 
 import { ThemeProvider } from 'styled-components/native';
 import theme from './styles/theme';
@@ -23,7 +24,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <CarDetails />
+      <Scheduling />
     </ThemeProvider>
   );
 }
