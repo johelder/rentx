b8ff6aaf1ef1d74fac33945879d847303d99f862
johelder <johelder.arj@gmail.com>
2021-10-29 23:25:09 -0300

    create confirm button component

---
+++ src/components/ConfirmButton/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { RectButtonProps } from 'react-native-gesture-handler';
+
+import * as S from './styles';
+
+interface IConfirmButtonProps extends RectButtonProps {
+  title: string;
+}
+
+export function ConfirmButton({ title, ...rest }: IConfirmButtonProps) {
+  return (
+    <S.Container {...rest}>
+      <S.Title>{title}</S.Title>
+    </S.Container>
+  );
+}
