8d1ef2ed15287fe307a9f9345a285ce0508a29fc
johelder <johelder.arj@gmail.com>
2021-11-02 19:45:29 -0300

    adjustment in button component to use onPress method

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,11 +5,12 @@ import * as S from './styles';
 interface IButtonProps {
   title: string;
   color?: string;
+  onPress: () => void;
 }
 
-export function Button({ title, color, ...rest }: IButtonProps) {
+export function Button({ title, color, onPress }: IButtonProps) {
   return (
-    <S.Container color={color} {...rest}>
+    <S.Container color={color} onPress={onPress}>
       <S.Title>{title}</S.Title>
     </S.Container>
   );
