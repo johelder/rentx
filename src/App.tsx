c9ae8e1d56001812231eccf4b40e20af240332a2
johelder <johelder.arj@gmail.com>
2021-11-02 19:45:05 -0300

    using routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import React from 'react';
 
 import AppLoading from 'expo-app-loading';
 
+import { Routes } from './routes';
+
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo';
 
 import { ThemeProvider } from 'styled-components/native';
 import theme from './styles/theme';
-import { Routes } from './routes';
 
 export default function App() {
 
