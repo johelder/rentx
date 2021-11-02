e945550cc7c6c4d2d6f8fc28f4ba2cf3309f7197
johelder <johelder.arj@gmail.com>
2021-11-02 17:04:52 -0300

    create stack routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
 
 import AppLoading from 'expo-app-loading';
@@ -5,14 +7,9 @@ import AppLoading from 'expo-app-loading';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo';
 
-import { Home } from './pages/Home';
-import { CarDetails } from './pages/CarDetails';
-import { Scheduling } from './pages/Scheduling';
-import { SchedulingDetails } from './pages/SchedulingDetails';
-import { SchedulingComplete } from './pages/SchedulingComplete';
-
 import { ThemeProvider } from 'styled-components/native';
 import theme from './styles/theme';
+import { Routes } from './routes';
 
 export default function App() {
 
@@ -26,7 +23,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <SchedulingComplete />
+      <Routes />
     </ThemeProvider>
   );
 }
