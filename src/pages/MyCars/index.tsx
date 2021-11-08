64c653a3ca5573f4dddbd3b19c3ab2f7b9964225
johelder <johelder.arj@gmail.com>
2021-11-07 22:05:06 -0300

    refactoring car card component

--- src/pages/MyCars/index.tsx
+++ src/pages/MyCars/index.tsx
@@ -1,21 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { StatusBar, FlatList } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StatusBar, FlatList } from "react-native";
 
-import { useNavigation } from '@react-navigation/native';
-import { useTheme } from 'styled-components';
+import { useNavigation } from "@react-navigation/native";
+import { useTheme } from "styled-components";
 
-import { BackButton } from '../../components/BackButton';
+import { BackButton } from "../../components/BackButton";
+import { Car } from "../../components/Car";
+import { Load } from "../../components/Load";
 
-import { CarDTO } from '../../dtos/CarDTO';
-import { api } from '../../services/api';
+import { CarDTO } from "../../dtos/CarDTO";
+import { api } from "../../services/api";
 
-import * as S from './styles';
-import { Car } from '../../components/Car';
+import { AntDesign } from "@expo/vector-icons";
+
+import * as S from "./styles";
 
 interface ICarProps {
   id: string;
   user_id: string;
   car: CarDTO;
+  startDate: string;
+  endDate: string;
 }
 
 export function MyCars() {
@@ -27,7 +32,7 @@ export function MyCars() {
 
   async function getCars() {
     try {
-      const response = await api.get('schedules_byuser?user_id=1');
+      const response = await api.get("schedules_byuser?user_id=1");
       setMyCars(response.data);
     } catch (error) {
       throw new Error(error.message);
@@ -40,14 +45,13 @@ export function MyCars() {
     navigation.goBack();
   }
 
-
   useEffect(() => {
     getCars();
   }, []);
 
   return (
     <S.Container>
-       <StatusBar
+      <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
@@ -55,30 +59,45 @@ export function MyCars() {
       <S.Header>
         <BackButton color={theme.colors.shape} onPress={handleBack} />
 
-        <S.Title>
-          Seus agendamentos, estão aqui.
-        </S.Title>
+        <S.Title>Seus agendamentos, estão aqui.</S.Title>
 
-        <S.SubTitle>
-          Conforto, segurança e praticidade.
-        </S.SubTitle>
+        <S.SubTitle>Conforto, segurança e praticidade.</S.SubTitle>
       </S.Header>
 
-      <S.Content>
-        <S.Appointments>
-          <S.AppointmentsTitle>Agendamentos feitos</S.AppointmentsTitle>
-          <S.AppointmentsQuantity>06</S.AppointmentsQuantity>
-        </S.Appointments>
+      {loading ? (
+        <Load />
+      ) : (
+        <S.Content>
+          <S.Appointments>
+            <S.AppointmentsTitle>Agendamentos feitos</S.AppointmentsTitle>
+            <S.AppointmentsQuantity>{myCars.length}</S.AppointmentsQuantity>
+          </S.Appointments>
 
-        <FlatList
-          data={myCars}
-          keyExtractor={item => String(item.id)}
-          showsVerticalScrollIndicator={false}
-          renderItem={({ item }) => (
-            <Car data={item.car} />
-          )}
-        />
-      </S.Content>
+          <FlatList
+            data={myCars}
+            keyExtractor={(item) => String(item.id)}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <S.CarWrapper>
+                <Car data={item.car} />
+                <S.CarFooter>
+                  <S.CarFooterTitle>Período</S.CarFooterTitle>
+                  <S.CarFooterPeriod>
+                    <S.CarFooterDate>{item.startDate}</S.CarFooterDate>
+                    <AntDesign
+                      name="arrowright"
+                      size={20}
+                      color={theme.colors.text}
+                      style={{ marginHorizontal: 10 }}
+                    />
+                    <S.CarFooterDate>{item.endDate}</S.CarFooterDate>
+                  </S.CarFooterPeriod>
+                </S.CarFooter>
+              </S.CarWrapper>
+            )}
+          />
+        </S.Content>
+      )}
     </S.Container>
   );
 }
--- src/pages/MyCars/styles.ts
+++ src/pages/MyCars/styles.ts
@@ -62,3 +62,39 @@ export const AppointmentsQuantity = styled.Text`
 
   color: ${({ theme }) => theme.colors.title};
 `;
+
+export const CarWrapper = styled.View`
+  width: 100%;
+  margin-bottom: 16px;
+`;
+
+export const CarFooter = styled.View`
+  flex-direction: row;
+
+  align-items: center;
+  justify-content: space-between;
+
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+  margin-top: -10px;
+  padding: 12px 24px;
+`;
+
+export const CarFooterTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  font-size: ${RFValue(10)}px;
+
+  color: ${({ theme }) => theme.colors.text};
+
+  text-transform: uppercase;
+`;
+
+export const CarFooterPeriod = styled.View`
+  flex-direction: row;
+`;
+
+export const CarFooterDate = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  font-size: ${RFValue(13)}px;
+
+  color: ${({ theme }) => theme.colors.title};
+`;
