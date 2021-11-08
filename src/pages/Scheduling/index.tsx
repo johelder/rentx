5318a9f5e64066414fb928f395562859a5e8c3eb
johelder <johelder.arj@gmail.com>
2021-11-07 22:05:37 -0300

    formating schedule page

--- src/pages/Scheduling/index.tsx
+++ src/pages/Scheduling/index.tsx
@@ -1,21 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { Alert, StatusBar } from 'react-native';
+import { Alert, StatusBar } from "react-native";
 
-import { format } from 'date-fns';
-import { getPlataformDate } from '../../utils/getPlataformDate';
+import { format } from "date-fns";
+import { getPlataformDate } from "../../utils/getPlataformDate";
 
-import { useNavigation, useRoute } from '@react-navigation/native';
-import { useTheme } from 'styled-components';
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { useTheme } from "styled-components";
 
-import { BackButton } from '../../components/BackButton';
-import { Button } from '../../components/Button';
-import { Calendar, IDateObject, IMarkedDateProps, generateInterval } from '../../components/Calendar';
+import { BackButton } from "../../components/BackButton";
+import { Button } from "../../components/Button";
+import {
+  Calendar,
+  IDateObject,
+  IMarkedDateProps,
+  generateInterval,
+} from "../../components/Calendar";
 
-import ArrowIcon from '../../assets/arrow.svg';
+import ArrowIcon from "../../assets/arrow.svg";
 
-import * as S from './styles';
-import { CarDTO } from '../../dtos/CarDTO';
+import * as S from "./styles";
+import { CarDTO } from "../../dtos/CarDTO";
 
 interface IRentalPeriodProps {
   startDayFormatted: string;
@@ -27,9 +32,15 @@ interface IRouteParams {
 }
 
 export function Scheduling() {
-  const [lastSelectedDate, setLastSelectedDate] = useState<IDateObject>({} as IDateObject);
-  const [markedDates, setMarkedDates] = useState<IMarkedDateProps>({} as IMarkedDateProps);
-  const [rentalPeriod, setRentalPeriod] = useState<IRentalPeriodProps>({} as IRentalPeriodProps);
+  const [lastSelectedDate, setLastSelectedDate] = useState<IDateObject>(
+    {} as IDateObject
+  );
+  const [markedDates, setMarkedDates] = useState<IMarkedDateProps>(
+    {} as IMarkedDateProps
+  );
+  const [rentalPeriod, setRentalPeriod] = useState<IRentalPeriodProps>(
+    {} as IRentalPeriodProps
+  );
 
   const navigation = useNavigation();
   const theme = useTheme();
@@ -37,12 +48,10 @@ export function Scheduling() {
   const { car } = route.params as IRouteParams;
 
   function handleNavigate() {
-
-    if(!rentalPeriod.startDayFormatted || !rentalPeriod.endingDayFormatted) {
-      return Alert.alert('Selecione um período de aluguel.');
-    }
-
-    navigation.navigate('SchedulingDetails', { car, dates: Object.keys(markedDates) });
+    navigation.navigate("SchedulingDetails", {
+      car,
+      dates: Object.keys(markedDates),
+    });
   }
 
   function handleBack() {
@@ -53,7 +62,7 @@ export function Scheduling() {
     let startDay = !lastSelectedDate.timestamp ? date : lastSelectedDate;
     let endingDay = date;
 
-    if(startDay.timestamp > endingDay.timestamp) {
+    if (startDay.timestamp > endingDay.timestamp) {
       startDay = endingDay;
       endingDay = startDay;
     }
@@ -64,12 +73,19 @@ export function Scheduling() {
     setMarkedDates(interval);
 
     const firstDateSelected = Object.keys(interval)[0];
-    const lastDateSelected = Object.keys(interval)[Object.keys(interval).length - 1];
+    const lastDateSelected =
+      Object.keys(interval)[Object.keys(interval).length - 1];
 
     setRentalPeriod({
-      startDayFormatted: format(getPlataformDate(new Date(firstDateSelected)), 'dd/MM/yyyy'),
-      endingDayFormatted: format(getPlataformDate(new Date(lastDateSelected)), 'dd/MM/yyyy'),
-    })
+      startDayFormatted: format(
+        getPlataformDate(new Date(firstDateSelected)),
+        "dd/MM/yyyy"
+      ),
+      endingDayFormatted: format(
+        getPlataformDate(new Date(lastDateSelected)),
+        "dd/MM/yyyy"
+      ),
+    });
   }
 
   return (
@@ -83,22 +99,26 @@ export function Scheduling() {
         <BackButton color={theme.colors.shape} onPress={handleBack} />
 
         <S.Title>
-          Escolha uma {'\n'}
-          data de início e {'\n'}
+          Escolha uma {"\n"}
+          data de início e {"\n"}
           fim do aluguel
         </S.Title>
 
         <S.RentPeriod>
           <S.DateInfo>
             <S.DateTitle>DE</S.DateTitle>
-            <S.DateValue selected={!!rentalPeriod.startDayFormatted}>{rentalPeriod.startDayFormatted}</S.DateValue>
+            <S.DateValue selected={!!rentalPeriod.startDayFormatted}>
+              {rentalPeriod.startDayFormatted}
+            </S.DateValue>
           </S.DateInfo>
-        
+
           <ArrowIcon />
 
           <S.DateInfo>
             <S.DateTitle>ATÉ</S.DateTitle>
-            <S.DateValue selected={!!rentalPeriod.endingDayFormatted}>{rentalPeriod.endingDayFormatted}</S.DateValue>
+            <S.DateValue selected={!!rentalPeriod.endingDayFormatted}>
+              {rentalPeriod.endingDayFormatted}
+            </S.DateValue>
           </S.DateInfo>
         </S.RentPeriod>
       </S.Header>
@@ -108,7 +128,11 @@ export function Scheduling() {
       </S.Content>
 
       <S.Footer>
-        <Button title="Confirmar" onPress={handleNavigate} />
+        <Button
+          title="Confirmar"
+          onPress={handleNavigate}
+          enabled={!!rentalPeriod.startDayFormatted}
+        />
       </S.Footer>
     </S.Container>
   );
