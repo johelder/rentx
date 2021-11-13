1e48b6c90fe7f76593187a388e2f90224d3fff3d
johelder <johelder.arj@gmail.com>
2021-11-13 18:30:34 -0300

    animation of my cars button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,19 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, StyleSheet } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
-import { Ionicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 import { useTheme } from "styled-components";
+import { RectButton, PanGestureHandler } from "react-native-gesture-handler";
+
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  useAnimatedGestureHandler,
+  withSpring
+} from "react-native-reanimated";
+const AnimatedButton = Animated.createAnimatedComponent(RectButton);
 
 import { api } from "../../services/api";
 import { CarDTO } from "../../dtos/CarDTO";
@@ -23,12 +32,39 @@ export function Home() {
   const navigation = useNavigation();
   const theme = useTheme();
 
+  const positionX = useSharedValue(0);
+  const positionY = useSharedValue(0);
+
+  const myCarButtonStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: positionX.value },
+        { translateY: positionY.value },
+      ],
+    };
+  });
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart(_, ctx: any) {
+      ctx.positionX = positionX.value;
+      ctx.positionY = positionY.value;
+    },
+    onActive(event, ctx: any) {
+      positionX.value = event.translationX + ctx.positionX;
+      positionY.value = event.translationY + ctx.positionY;
+    },
+    onEnd() {
+      positionX.value = withSpring(0);
+      positionY.value = withSpring(0);
+    },
+  });
+
   function handleNavigate(car: CarDTO) {
-    navigation.navigate('CarDetails', { car });
+    navigation.navigate("CarDetails", { car });
   }
 
   function handleMyCars() {
-    navigation.navigate('MyCars');
+    navigation.navigate("MyCars");
   }
 
   const fetchCars = useCallback(async () => {
@@ -66,17 +102,42 @@ export function Home() {
         <S.CarList
           data={cars}
           keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <Car data={item} onPress={() => handleNavigate(item)} />}
+          renderItem={({ item }) => (
+            <Car data={item} onPress={() => handleNavigate(item)} />
+          )}
         />
       )}
 
-      <S.MyCarsButton onPress={handleMyCars}>
-        <Ionicons 
-          name="car-sport"
-          size={32}
-          color={theme.colors.background_secondary}
-        />
-      </S.MyCarsButton>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View
+          style={[
+            myCarButtonStyle,
+            { position: "absolute", bottom: 22, right: 16 },
+          ]}
+        >
+          <AnimatedButton
+            style={[styles.button, { backgroundColor: theme.colors.main }]}
+            onPress={handleMyCars}
+          >
+            <Ionicons
+              name="car-sport"
+              size={32}
+              color={theme.colors.background_secondary}
+            />
+          </AnimatedButton>
+        </Animated.View>
+      </PanGestureHandler>
     </S.Container>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: 60,
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+
+    borderRadius: 30,
+  },
+});
