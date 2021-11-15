5750b8ae11820306173a5c061175fb5ce46329b5
johelder <johelder.arj@gmail.com>
2021-11-15 15:58:49 -0300

    adding focus and filled logic

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextInputProps } from "react-native";
 
 import Feather from "@expo/vector-icons/Feather";
@@ -9,18 +9,40 @@ import * as S from "./styles";
 
 interface IInputProps extends TextInputProps {
   iconName: React.ComponentProps<typeof Feather>["name"];
+  value?: string;
 }
 
-export function Input({ iconName, ...rest }: IInputProps) {
+export function Input({ iconName, value, ...rest }: IInputProps) {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
   const theme = useTheme();
+
+  function handleInputFocus() {
+    setIsFocused(true);
+  }
+
+  function handleInputBlur() {
+    setIsFocused(false);
+    setIsFilled(!!value);
+  }
 
   return (
     <S.Container>
-      <S.IconContainer>
-        <Feather name={iconName} color={theme.colors.text} size={24} />
+      <S.IconContainer isFocused={isFocused}>
+        <Feather
+          name={iconName}
+          color={isFocused || isFilled ? theme.colors.main : theme.colors.text}
+          size={24}
+        />
       </S.IconContainer>
 
-      <S.InputText {...rest} />
+      <S.InputText
+        isFocused={isFocused}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        {...rest}
+      />
     </S.Container>
   );
 }
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,12 +1,18 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { TextInput } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
+interface IProps {
+  isFocused: boolean;
+}
+
 export const Container = styled.View`
   flex-direction: row;
+
+  margin-bottom: 8px;
 `;
 
-export const IconContainer = styled.View`
+export const IconContainer = styled.View<IProps>`
   width: 55px;
   height: 55px;
 
@@ -16,16 +22,30 @@ export const IconContainer = styled.View`
   margin-right: 2px;
 
   background-color: ${({ theme }) => theme.colors.background_secondary};
+
+  ${({ theme, isFocused }) =>
+    isFocused &&
+    css`
+      border-bottom-width: 2px;
+      border-bottom-color: ${theme.colors.main};
+    `}
 `;
 
-export const InputText = styled(TextInput)`
+export const InputText = styled(TextInput)<IProps>`
   flex: 1;
 
   font-size: ${RFValue(15)}px;
-  font-family: ${({theme}) => theme.fonts.primary_400};
+  font-family: ${({ theme }) => theme.fonts.primary_400};
 
   padding: 0 24px;
 
   background-color: ${({ theme }) => theme.colors.background_secondary};
   color: ${({ theme }) => theme.colors.text_detail};
+
+  ${({ theme, isFocused }) =>
+    isFocused &&
+    css`
+      border-bottom-width: 2px;
+      border-bottom-color: ${theme.colors.main};
+    `}
 `;
