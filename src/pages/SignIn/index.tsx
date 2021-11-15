8f42c345767f14ddf19a73770bbd19a589b1ba86
johelder <johelder.arj@gmail.com>
2021-11-15 15:59:11 -0300

    create inputs states

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,58 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  StatusBar,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
+import { PasswordInput } from "../../components/PasswordInput";
 
 import { useTheme } from "styled-components";
 
 import * as S from "./styles";
-import { StatusBar } from "react-native";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const theme = useTheme();
 
   return (
-    <S.Container>
-      <StatusBar
-        barStyle="dark-content"
-        backgroundColor="transparent"
-        translucent
-      />
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <S.Container>
+          <StatusBar
+            barStyle="dark-content"
+            backgroundColor="transparent"
+            translucent
+          />
 
-      <S.Header>
-        <S.Title>Estamos{"\n"}quase lá.</S.Title>
+          <S.Header>
+            <S.Title>Estamos{"\n"}quase lá.</S.Title>
 
-        <S.SubTitle>
-          Faça seu login para começar{"\n"}uma experência incrível.
-        </S.SubTitle>
-      </S.Header>
+            <S.SubTitle>
+              Faça seu login para começar{"\n"}uma experência incrível.
+            </S.SubTitle>
+          </S.Header>
 
-      <S.Form>
-        <Input
-          iconName="mail"
-          placeholder="E-mail"
-          autoCapitalize="none"
-          autoCorrect={false}
-          keyboardType="email-address"
-        />
-      </S.Form>
+          <S.Form>
+            <Input
+              iconName="mail"
+              placeholder="E-mail"
+              autoCapitalize="none"
+              autoCorrect={false}
+              keyboardType="email-address"
+              onChangeText={setEmail}
+              value={email}
+            />
 
-      <S.Footer>
-        <Button
-          title="Login"
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        />
+            <PasswordInput
+              iconName="lock"
+              placeholder="Senha"
+              onChangeText={setPassword}
+              value={password}
+            />
+          </S.Form>
 
-        <Button
-          title="Criar conta gratuita"
-          color={theme.colors.background_secondary}
-          onPress={() => {}}
-          loading={false}
-          light
-        />
-      </S.Footer>
-    </S.Container>
+          <S.Footer>
+            <Button
+              title="Login"
+              onPress={() => {}}
+              enabled={false}
+              loading={false}
+            />
+
+            <Button
+              title="Criar conta gratuita"
+              color={theme.colors.background_secondary}
+              onPress={() => {}}
+              loading={false}
+              light
+            />
+          </S.Footer>
+        </S.Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
