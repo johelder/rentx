c1d9cf70c39687de0e654e3df112bfdae15a4810
johelder <johelder.arj@gmail.com>
2021-11-16 22:14:14 -0300

    create first step input validation

--- src/pages/SignUp/SignUpFirstStep/index.tsx
+++ src/pages/SignUp/SignUpFirstStep/index.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
   StatusBar,
+  Alert,
 } from "react-native";
+
+import * as Yup from "yup";
 
 import { useNavigation } from "@react-navigation/native";
 
@@ -16,16 +19,40 @@ import { Input } from "../../../components/Input";
 import * as S from "./styles";
 
 export function SignUpFirstStep() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [driverLicensed, setDriverLicensed] = useState("");
 
-  const { goBack } = useNavigation();
+  const { goBack, navigate } = useNavigation();
+
+  async function handleNextStep() {
+    try {
+      const schema = Yup.object().shape({
+        driverLicensed: Yup.string().required("CNH obrigatória"),
+        email: Yup.string()
+          .required("E-mail obrigatório")
+          .email("Informe um e-mail válido"),
+        name: Yup.string().required("Nome obrigatório"),
+      });
+
+      const data = { name, email, driverLicensed };
+      await schema.validate(data);
+
+      navigate("SignUpSecondStep", { user: data });
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        return Alert.alert("Peraí", error.message);
+      }
+    }
+  }
 
   return (
     <KeyboardAvoidingView behavior="position" enabled>
       <StatusBar
-          barStyle="dark-content"
-          backgroundColor="transparent"
-          translucent
-        />
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <S.Container>
           <S.Header>
@@ -45,11 +72,28 @@ export function SignUpFirstStep() {
           <S.Form>
             <S.FormTitle>1. Dados</S.FormTitle>
 
-            <Input iconName="user" placeholder="Nome" />
-            <Input iconName="mail" placeholder="E-mail" />
-            <Input iconName="credit-card" placeholder="CNH" />
+            <Input
+              iconName="user"
+              placeholder="Nome"
+              onChangeText={setName}
+              value={name}
+            />
+            <Input
+              iconName="mail"
+              placeholder="E-mail"
+              keyboardType="email-address"
+              onChangeText={setEmail}
+              value={email}
+            />
+            <Input
+              iconName="credit-card"
+              placeholder="CNH"
+              keyboardType="numeric"
+              onChangeText={setDriverLicensed}
+              value={driverLicensed}
+            />
 
-            <Button title="Próximo" />
+            <Button title="Próximo" onPress={handleNextStep} />
           </S.Form>
         </S.Container>
       </TouchableWithoutFeedback>
