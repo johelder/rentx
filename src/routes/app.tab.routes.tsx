bce8484c2b731af1459291663b89eab11d863b58
johelder <johelder.arj@gmail.com>
2021-11-21 12:17:20 -0300

    create app tab routes

---
+++ src/routes/app.tab.routes.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Platform } from "react-native";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { AppStackRoutes } from "./app.stack.routes";
+import { Home } from "../pages/Home";
+import { MyCars } from "../pages/MyCars";
+
+import HomeIcon from "../assets/home.svg";
+import CarIcon from "../assets/car.svg";
+import ProfileIcon from "../assets/people.svg";
+
+import { useTheme } from "styled-components";
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export function AppTabRoutes() {
+  const theme = useTheme();
+
+  return (
+    <Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        activeTintColor: theme.colors.main,
+        inactiveTintColor: theme.colors.text_detail,
+        showLabel: false,
+        style: {
+          paddingVertical: Platform.OS === "ios" ? 20 : 0,
+          height: 78,
+          backgroundColor: theme.colors.background_primary,
+        },
+      }}
+    >
+      <Screen
+        name="Home"
+        component={AppStackRoutes}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeIcon width={24} height={24} fill={color} />
+          ),
+        }}
+      />
+
+      <Screen
+        name="MyCars"
+        component={MyCars}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <CarIcon width={24} height={24} fill={color} />
+          ),
+        }}
+      />
+
+      <Screen
+        name="Profile"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ProfileIcon width={24} height={24} fill={color} />
+          ),
+        }}
+      />
+    </Navigator>
+  );
+}
