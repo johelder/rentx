7eff52ef8dd408d416666c6e7bf894477a351911
johelder <johelder.arj@gmail.com>
2021-11-21 14:53:27 -0300

    configure connect watermelondb

---
+++ src/database/index.ts
@@ -0,0 +1,16 @@
+import { Database } from "@nozbe/watermelondb";
+import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
+
+import { User } from "./model/User";
+import { schemas } from "./schema";
+
+const adapter = new SQLiteAdapter({
+  schema: schemas,
+});
+
+const database = new Database({
+  adapter,
+  modelClasses: [ User ],
+});
+
+export { database };
